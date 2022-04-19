4e5577b19acc820daf4f15c3c476770102cd69ca
boost201106 <magnuskarik@gmail.com>
2022-04-19 10:14:25 +0000

    did some stuff

---
+++ script.js
@@ -0,0 +1,8 @@
+var uuid="3e04285e-207d-4d8b-ba91-c0793542c31a"
+var url="https://api.hypixel.net/player?uuid="+uuid+"&key="+api
+fetch(url)
+.then(result => result.json())
+.then(({ player }) => {
+// Log the player's username
+console.log(player.displayname)
+})
