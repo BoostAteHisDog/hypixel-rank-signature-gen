4532425d596fdf4ba7cb4d6061c8d7072fa74be9
boost201106 <magnuskarik@gmail.com>
2022-04-19 11:07:31 +0000

    E

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var uuid="3e04285e-207d-4d8b-ba91-c0793542c31a"
-var url="https://api.hypixel.net/player?uuid="+uuid+"&key="+API
+var url="https://api.hypixel.net/player?uuid="+uuid+"&key="+API_KEY
+var API_KEY=steps.env.API_KEY
 fetch(url)
 .then(result => result.json())
 .then(({ player }) => {
