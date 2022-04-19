471c22c7875a607117e64ced9a9a6cfd658b3d9e
boost201106 <magnuskarik@gmail.com>
2022-04-19 10:25:36 +0000

    fixed capitalization on API key

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var uuid="3e04285e-207d-4d8b-ba91-c0793542c31a"
-var url="https://api.hypixel.net/player?uuid="+uuid+"&key="+api
+var url="https://api.hypixel.net/player?uuid="+uuid+"&key="+API
 fetch(url)
 .then(result => result.json())
 .then(({ player }) => {
